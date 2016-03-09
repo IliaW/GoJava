a6cbd9f7af5241fe8f1aaa68d1b05c1777f719f8
Ilia Vovk <vovkilia@gmail.com>
2016-03-09 15:12:24 +0200

    added methods main

---
+++ mod3/src/flowers/Main.java
@@ -0,0 +1,15 @@
+package flowers;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Flowers aster = new Aster();
+        Flowers chamomile = new Chamomile();
+        Flowers rose = new Rose();
+        Flowers tulip = new Tulip();
+        Bouquet bouquet = new Bouquet();
+        Rosebush rosebush = new Rosebush();
+
+    }
+}
---
+++ mod3/src/instrument/Main.java
@@ -0,0 +1,13 @@
+package instrument;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MusicalInstrument guitar = new Guitar();
+        MusicalInstrument piano = new Piano();
+        MusicalInstrument trumpet = new Trumpet();
+        MusicShop musicShop = new MusicShop();
+
+    }
+}
