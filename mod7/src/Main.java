5e6c21640590db1514fe6c961b6e0b643e4df165
Ilia Vovk <vovkilia@gmail.com>
2016-03-24 18:37:01 +0200

    changed the magic number

--- mod7/src/Main.java
+++ mod7/src/Main.java
@@ -4,11 +4,11 @@ import java.util.InputMismatchException;
 public class Main {
     public static void main(String[] args) {
         try {
-            ScanRandomSort a = new ScanRandomSort();
+            ScanRandomSort scanRandomSort = new ScanRandomSort();
             System.out.println("Введіть довжину масива");
-            a.scanAndRandom();
-            System.out.println("Заданий масив       " + Arrays.toString(a.array));
-            System.out.println("Відсортований масив " + Arrays.toString(a.sorting(a.array)));
+            scanRandomSort.scanAndRandom();
+            System.out.println("Заданий масив       " + Arrays.toString(scanRandomSort.array));
+            System.out.println("Відсортований масив " + Arrays.toString(scanRandomSort.sorting(scanRandomSort.array)));
         } catch (InputMismatchException e) {
             System.out.println("Невірний формат вводу");
         }
