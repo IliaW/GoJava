36a8cb74bbc8e323980e8cd86bc9d0c823f765fc
Ilia Vovk <vovkilia@gmail.com>
2016-04-04 22:11:39 +0300

    refactor Caesar

---
+++ Test/src/NewCaesarFromIlia/Caesar.java
@@ -0,0 +1,29 @@
+package NewCaesarFromIlia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Caesar {
+
+    private List<Character> alphabet = new ArrayList<>();
+
+    Caesar() {
+        for (char c = ' '; c <= '~'; c++) {                 // Loading all ASCII characters
+            alphabet.add(c);
+        }
+    }
+
+    public String encode(String text, int key) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            int index = alphabet.indexOf(text.charAt(i)); // Search the library letter and extracting of her index
+            index = (index + key) % alphabet.size();      // Encryption
+            builder.append(alphabet.get(index));
+        }
+        return builder.toString();
+    }
+
+    public String decrypt(String text, int key) {
+        return encode(text, -key);
+    }
+}
---
+++ Test/src/NewCaesarFromIlia/Main.java
@@ -0,0 +1,27 @@
+package NewCaesarFromIlia;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Encoding the text
+        Scanner scanner = new Scanner(System.in);
+        Caesar caesar = new Caesar();
+        System.out.println("Enter the text you want to decrypt");
+        String inputText = scanner.nextLine();
+        System.out.println("Enter the key");
+        int inputKey = scanner.nextInt();
+        String outText = caesar.encode(inputText, inputKey);
+        System.out.println("\nCiphertext:\n" + outText);
+
+        // Decoding the text
+        System.out.println("\nEnter the key to decrypt");
+        int key = scanner.nextInt();
+        String decryptText = caesar.decrypt(outText, key);
+        if (inputText.equals(decryptText))
+            System.out.println(decryptText);
+        else
+            System.out.println("Incorrect key");
+    }
+}
